f8574549594f0f060cb17956d49e78c3eefb710b
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-09 17:05:54 +0630

    how-to-use-image-in-react

--- create-react-app/spa-react/src/App.js
+++ create-react-app/spa-react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './assets/image.jpg';
 
 function App() {
+  
+  let name = "Aung Pyae Sone";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Hello {name}</h1>
+      <img src={Image} alt="This is an img!"/>
+    </>
+  )
 }
 
 export default App;
