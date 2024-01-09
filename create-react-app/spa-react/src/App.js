53c865c2e33ea0cff29d028b4234a1b53ff329f1
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-09 17:27:08 +0630

    useState

--- create-react-app/spa-react/src/App.js
+++ create-react-app/spa-react/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Image from './assets/image.jpg';
+import { useState } from 'react';
 
 function App() {
   
-  let name = "Aung Pyae Sone";
+  // let name = "Aung Pyae Sone";
+  let [name,setName] = useState("Aung Pyae Sone");   // useState(prev(or)default,new)  (or) useState(getter,setter)
+
+  let changeName = () => {
+    setName("Aung Aung");
+    console.log(name);
+  }
 
   return (
     <div className="app">
       <h1>Hello {name}</h1>
       <img src={Image} alt="This is an img!"/>
+      <button onClick={changeName}>Change Name</button>
     </div>
   )
 }
