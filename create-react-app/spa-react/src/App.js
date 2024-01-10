5a451e7dadb85544c5b44986c8897917653b92e6
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-10 16:03:29 +0630

    working-with-list

--- create-react-app/spa-react/src/App.js
+++ create-react-app/spa-react/src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function App() {
   
-  // let name = "Aung Pyae Sone";
+  // useState
   let [name,setName] = useState("Aung Pyae Sone");   // useState(prev(or)default,new)  (or) useState(getter,setter)
 
   let changeName = () => {
@@ -12,11 +12,35 @@ function App() {
     console.log(name);
   }
 
+  // list
+  let [posts,setPosts] = useState([
+    {
+      id : 1,
+      title : 'First post'
+    },
+    {
+      id : 2,
+      title : 'second post'
+    },
+    {
+      id : 3,
+      title : 'third post'
+    }
+  ]);
+  console.log(posts);
+
   return (
     <div className="app">
       <h1>Hello {name}</h1>
       <img src={Image} alt="This is an img!"/>
       <button onClick={changeName}>Change Name</button>
+      <br/>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
