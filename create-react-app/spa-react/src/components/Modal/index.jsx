8dc629f22c3ea7074706a25e992f037baffda589
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-14 20:12:44 +0630

    inline-css

--- create-react-app/spa-react/src/components/Modal/index.jsx
+++ create-react-app/spa-react/src/components/Modal/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './index.css';
 import ReactDom from 'react-dom';
 
-const Modal = ({ children }) => {
+const Modal = ({ children, danger = false }) => {
     return (
         ReactDom.createPortal(
             <div className="modal-component">
                 <div className='modal-backdrop'>
-                    <div className="modal">
+                    <div className="modal" style={{
+                        border : '4px solid',
+                        borderColor : danger ? 'red' : 'blue'
+                    }}>
                         {/* modal content  */}
                         {children}
                     </div>
