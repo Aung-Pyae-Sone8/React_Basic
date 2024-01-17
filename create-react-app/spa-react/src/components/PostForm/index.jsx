fde27d5424efb5280546b1fe678b05c07a5d739a
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-17 17:06:12 +0630

    two-way-data-binding

--- create-react-app/spa-react/src/components/PostForm/index.jsx
+++ create-react-app/spa-react/src/components/PostForm/index.jsx
@@ -5,14 +5,20 @@ const PostFrom = () => {
 
     let [title,setTitle] = useState("");
 
+    let resetForm = () => {
+        setTitle('');
+        console.log('updated successfully');
+    }
+
     return (
         <form className='post-form'>
             <h1>Create Post</h1>
             <div className="form-control">
                 <label htmlFor="">Title</label>
-                <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+                <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
             </div>
             <p>{title}</p>
+            <button type="button" onClick={resetForm}> reset form</button>
             <div className="form-control">
                 <button>Post Now</button>
             </div>
